12629b96d82e776c8954829a0dc44e8b5cfab306
dml <dml1630@163.com>
2018-07-07 17:45:45 +0800

    code

--- Example/MLIndicatorView/ViewController.swift
+++ Example/MLIndicatorView/ViewController.swift
@@ -9,14 +9,37 @@
 import UIKit
 import MLIndicatorView
 class ViewController: UIViewController {
-
+    var indicatorView_OneStyle:MLIndicatorView! = nil
+    var indicatorView_TwoStyle:MLIndicatorView! = nil
+    var indicatorView_ThreeStyle:MLIndicatorView! = nil
+    var indicatorView_FourStyle:MLIndicatorView! = nil
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         let indicatorView = MLIndicatorView.init(frame: CGRect.init(x: 100, y: 100, width: 30  , height: 30), tintColor: UIColor.red, indicatorStyle: .MLIndicatorStyleCyclingLine, indicatorOptional: nil)
-         indicatorView.startIndicatorAnimation()
-        self.view.addSubview(indicatorView)
+        indicatorView_OneStyle = MLIndicatorView.init(frame: CGRect.init(x: 30, y: 100, width: 60, height: 60), tintColor: UIColor.red, indicatorStyle: .MLIndicatorStyleCyclingLine, indicatorOptional: nil)
+        self.view.addSubview(indicatorView_OneStyle)
+        
+        
+        indicatorView_TwoStyle = MLIndicatorView.init(frame: CGRect.init(x: (UIScreen.main.bounds.size.width - 60) * 0.5, y: 100, width: 60, height: 60), tintColor: UIColor.red, indicatorStyle: .MLIndicatorStyleCyclingRing, indicatorOptional: nil)
+        self.view.addSubview(indicatorView_TwoStyle)
+        
+        indicatorView_ThreeStyle = MLIndicatorView.init(frame: CGRect.init(x: 30, y: 200, width: 60 , height: 60), tintColor: UIColor.red, indicatorStyle: .MLIndicatorStyleCyclingScale, indicatorOptional: nil)
+        self.view.addSubview(indicatorView_ThreeStyle)
+        
+        indicatorView_FourStyle = MLIndicatorView.init(frame: CGRect.init(x: (UIScreen.main.bounds.size.width - 60) * 0.5, y: 225, width: 60 , height: 60), tintColor: UIColor.red, indicatorStyle: .MLIndicatorStyleTranslation, indicatorOptional: nil)
+        self.view.addSubview(indicatorView_FourStyle)
+        
+        
+        
     }
 
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        indicatorView_OneStyle.startIndicatorAnimation()
+        indicatorView_TwoStyle.startIndicatorAnimation()
+        indicatorView_ThreeStyle.startIndicatorAnimation()
+        indicatorView_FourStyle.startIndicatorAnimation()
+    }
+    
 }
 
--- MLIndicatorView/Classes/MLIndicatorView.swift
+++ MLIndicatorView/Classes/MLIndicatorView.swift
@@ -7,42 +7,47 @@
 
 import UIKit
 
-public enum MLIndicatorStyle {
+@objc public enum MLIndicatorStyle:Int {
     case MLIndicatorStyleCyclingLine
     case MLIndicatorStyleCyclingRing
-    case MLIndicatorStyleBounceSport
+    case MLIndicatorStyleCyclingScale
+    case MLIndicatorStyleTranslation
 }
 
-
-public struct MLIndicatorOptional {
+@objc open class MLIndicatorOptional: NSObject {
     // 线条宽度
     var lineWidth:CGFloat
     var reConut: Int
-    // 修改默认值
-    mutating func fixDefaultValue(lineWidth width:CGFloat ,reConut count:Int){
+    var itemSize: CGSize
+    var duration:CFTimeInterval
+    
+    
+    init(lineWidth width:CGFloat,reConut count:Int, size:CGSize, duration:CFTimeInterval) {
         self.reConut = count
         self.lineWidth = width
+        self.itemSize = size
+        self.duration = duration
+        super.init()
     }
-    
-    
+   
 }
 
 
-protocol MLIndicatorViewProtocol:NSObjectProtocol{
-    func setIndicatorAnimationWithSuperLayer(superLayer:CALayer, tintColor color:UIColor, indicatorSize size:CGSize,indicatorOptional optional:MLIndicatorOptional?)
-    func removeAnimation()
+@objc protocol MLIndicatorViewProtocol{
+   @objc func setIndicatorAnimationWithSuperLayer(superLayer:CALayer, tintColor color:UIColor, indicatorSize size:CGSize,indicatorOptional optional:MLIndicatorOptional?)
+    @objc func removeAnimation()
     
 }
 
 open class MLIndicatorView: UIView {
 
-    
-    let str :[String]? = nil
     let indicatorStyle:MLIndicatorStyle
     var indicatorProtocol:MLIndicatorViewProtocol! = nil
     let indicatorColor:UIColor
     let indicatorFrame:CGRect
     let indicatorOptional:MLIndicatorOptional?
+    var object:AnyObject! = nil
+    var isAnimating:Bool // 是否正在动画
     
     
     
@@ -51,11 +56,11 @@ open class MLIndicatorView: UIView {
         indicatorColor = color
         indicatorStyle = style
         indicatorOptional = optional
+        isAnimating = false
         super.init(frame: frame)
         
-        let a: Set<String> = Set.init()
-        
-        
+        addObserver()
+
     }
     
     public convenience init(tintColor color: UIColor ,indicatorStyle style:MLIndicatorStyle ,indicatorOptional optional:MLIndicatorOptional?) {
@@ -69,6 +74,29 @@ open class MLIndicatorView: UIView {
     
     
     
+    func addObserver(){
+        NotificationCenter.default.removeObserver(self)
+        NotificationCenter.default.addObserver(self, selector: #selector(appWillEnterBackground), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(appWillBecomeActive), name: NSNotification.Name.UIApplicationWillEnterForeground, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(appWillBecomeActive), name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
+    }
+    
+    @objc func appWillEnterBackground(){
+        if isAnimating == true {
+            self.indicatorProtocol.removeAnimation()
+        }
+    }
+    
+    @objc func appWillBecomeActive(){
+        if isAnimating == true {
+            self.startIndicatorAnimation()
+        }
+    }
+    
+    deinit {
+       NotificationCenter.default.removeObserver(self)
+    }
+    
     /// 获取指示器对象根据类型值
     ///
     /// - Parameter style: 类型
@@ -79,56 +107,70 @@ open class MLIndicatorView: UIView {
         switch style {
         case .MLIndicatorStyleCyclingLine:
             object  = MLCyclingLine.init()
-            
-            
             break
         case .MLIndicatorStyleCyclingRing:
-            
+             object  = MLCyclingRing.init()
             break
-        case .MLIndicatorStyleBounceSport:
-            
+        case .MLIndicatorStyleCyclingScale:
+            object  = MLCyclingScale.init()
+            break
+        case .MLIndicatorStyleTranslation:
+            object  = MLTranslation.init()
             break
         }
         return object
-        
-//        if let indicatorProtocolTemp = self.indicatorProtocol {
-//            return indicatorProtocolTemp
-//        }else{ // 默认值
-//            return MLCyclingLine.init() as MLIndicatorViewProtocol
-//        }
     }
     
     
     
    /// 开始动画
     public func startIndicatorAnimation(){
+        // 这个值一定要清空
+         self.layer.sublayers = nil;
+        self.setIndicatorNormalState()
+        self.object = self.getIndicatorOptionalWithStyle(style: indicatorStyle)
+        self.indicatorProtocol = self.object as! MLIndicatorViewProtocol
         
-     
+        if self.object.responds(to: #selector(MLIndicatorViewProtocol.setIndicatorAnimationWithSuperLayer(superLayer:tintColor:indicatorSize:indicatorOptional:))){
+            self.indicatorProtocol.setIndicatorAnimationWithSuperLayer(superLayer: self.layer, tintColor: indicatorColor, indicatorSize: indicatorFrame.size, indicatorOptional: indicatorOptional)
+        }
+        self.isAnimating = true
         
-       let object = self.getIndicatorOptionalWithStyle(style: indicatorStyle)
-//        object.
-        
-        
-//        if object.responds(to: #selector(MLIndicatorViewProtocol.setIndicatorAnimationWithSuperLayer(superLayer:tintColor:indicatorSize:indicatorOptional:))) {
-//            print("实现")
-//        }else{
-//            print("未实现")
-//        }
-//
-        
-//        if (self.indicatorProtocol.responds(to: #selector(MLIndicatorViewProtocol.setIndicatorAnimationWithSuperLayer(superLayer:tintColor:indicatorSize:indicatorOptional:)))) {
-//
-//        }
-    
-        
-//        self.indicatorProtocol?.setIndicatorAnimationWithSuperLayer(superLayer: self.layer, tintColor: indicatorColor, indicatorSize: indicatorFrame.size, indicatorOptional: indicatorOptional)
         
     }
     
-   /// 停止动画
-   public func stopIndicatorAnimation(){
-        self.indicatorProtocol?.removeAnimation()
+    /// 停止动画
+    public func stopIndicatorAnimation(){
+        
+        if self.isAnimating == true {
+            self.indicatorProtocol = self.object as! MLIndicatorViewProtocol
+            
+            if self.object.responds(to: #selector(MLIndicatorViewProtocol.removeAnimation)){
+                self.indicatorProtocol.removeAnimation()
+                self.isAnimating = false
+                self.indicatorProtocol = nil
+                self.object = nil
+            }
+            self.setIndicatorToFadeOutState()
+            NotificationCenter.default.removeObserver(self)
+        }
     }
+    
+    
+    func setIndicatorNormalState(){
+        self.layer.backgroundColor = UIColor.clear.cgColor
+        self.layer.opacity = 1
+        self.layer.speed = 1
+    }
+    
+    func setIndicatorToFadeOutState(){
+        self.layer.backgroundColor = UIColor.clear.cgColor
+        self.layer.opacity = 0
+        self.layer.sublayers = nil
+    }
+    
+    
+   
     
 }
 
@@ -143,14 +185,14 @@ class MLCyclingLine: UIView,MLIndicatorViewProtocol {
         if let indicatorOptional = optional {
             optionalTem = indicatorOptional
         }else{
-            optionalTem = MLIndicatorOptional.init(lineWidth: 1, reConut: 15)
+            optionalTem = MLIndicatorOptional.init(lineWidth: 1, reConut: 15, size: CGSize.init(width: 5, height: 5), duration:  Double(15) / Double(10))
         }
         
-        let relayer = CAReplicatorLayer.init()
-        relayer.frame = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
-        relayer.instanceCount = optionalTem.reConut
-        relayer.position = CGPoint.init(x: 0, y: 0)
-        relayer.backgroundColor = UIColor.clear.cgColor
+        let reLayer = CAReplicatorLayer.init()
+        reLayer.frame = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+        reLayer.instanceCount = optionalTem.reConut
+        reLayer.position = CGPoint.init(x: size.width * 0.5, y: size.height * 0.5)
+        reLayer.backgroundColor = UIColor.clear.cgColor
         /**
          指定复制副本之间的延迟（以秒为单位）。动画。
          默认值为0.0，表示将同步添加到复制副本的任何动画。
@@ -181,59 +223,235 @@ class MLCyclingLine: UIView,MLIndicatorViewProtocol {
          replicatorLayer.instanceTransform = CATransform3DMakeRotation(angle, 0, 0, 1)
          so animation.duration = Double(optional.reConut) / Double(10) 需要对应
          */
-        relayer.instanceDelay =  (Double(optionalTem.reConut) / Double(10)) / Double(optionalTem.reConut)
+        reLayer.instanceDelay =  optionalTem.duration / Double(optionalTem.reConut)
         
         /// 每个复制层的角度值
         let angle = CGFloat.pi * 2 / CGFloat(optionalTem.reConut)
-        relayer.instanceTransform = CATransform3DMakeRotation(angle, 0, 0, 1)
+        reLayer.instanceTransform = CATransform3DMakeRotation(angle, 0, 0, 1)
         
-        superLayer.addSublayer(relayer)
-        self.addRealLayerToReLayer(layer: relayer, tintColor: color, indicatorSize: size, indicatorOptional: optionalTem)
+        superLayer.addSublayer(reLayer)
+        self.addRealLayerToReLayer(layer: reLayer, tintColor: color, indicatorSize: size, indicatorOptional: optionalTem)
         
     }
     
  
     func removeAnimation() {
-        realLayer.removeAnimation(forKey: "animation")
+        self.realLayer.removeAnimation(forKey: "animation")
     }
     
     
     func addRealLayerToReLayer(layer:CALayer, tintColor color: UIColor, indicatorSize size: CGSize, indicatorOptional optional: MLIndicatorOptional){
         
-        
-        realLayer = CAShapeLayer.init()
-        realLayer.bounds = CGRect.init(x: 0, y: 0, width: optional.lineWidth, height: size.width/6)
-        realLayer.position = CGPoint.init(x: size.width/2, y: 5)
-        realLayer.opacity = 0.9
-        realLayer.cornerRadius = 1
-        realLayer.shouldRasterize = true
-        realLayer.rasterizationScale = UIScreen.main.scale
-        realLayer.backgroundColor = color.cgColor
-        layer.addSublayer(realLayer)
+        self.realLayer = CAShapeLayer.init()
+        self.realLayer.bounds = CGRect.init(x: 0, y: 0, width: optional.lineWidth, height: size.width/6)
+        self.realLayer.position = CGPoint.init(x: size.width * 0.5, y: 5)
+        self.realLayer.opacity = 0.9
+        self.realLayer.cornerRadius = self.realLayer.bounds.size.width * 0.5
+        self.realLayer.shouldRasterize = true
+        self.realLayer.rasterizationScale = UIScreen.main.scale
+        self.realLayer.backgroundColor = color.cgColor
+        layer.addSublayer(self.realLayer)
         
         let animation = CABasicAnimation.init()
         animation.keyPath = "opacity"
         animation.fromValue = 0
         animation.toValue = 0.9
         
-        animation.duration = Double(optional.reConut) / Double(10)
+        animation.duration = optional.duration
         animation.repeatCount = MAXFLOAT
         
-        realLayer.add(animation, forKey: "animation")
+        self.realLayer.add(animation, forKey: "animation")
+ 
+    }
+    
+}
+
+
+
+
+class MLCyclingRing: UIView,MLIndicatorViewProtocol {
+    
+    var cyclingRinglayer:CAShapeLayer! = nil
+    func setIndicatorAnimationWithSuperLayer(superLayer: CALayer, tintColor color: UIColor, indicatorSize size: CGSize, indicatorOptional optional: MLIndicatorOptional?) {
+        
+        let optionalTem:MLIndicatorOptional
+        if let indicatorOptional = optional {
+            optionalTem = indicatorOptional
+        }else{
+            optionalTem = MLIndicatorOptional.init(lineWidth: 1, reConut: 15, size: CGSize.init(width: 5, height: 5), duration: 1)
+        }
+        cyclingRinglayer = CAShapeLayer.init()
+        cyclingRinglayer.frame = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+        cyclingRinglayer.opacity = 1;
+        cyclingRinglayer.fillColor = UIColor.clear.cgColor
+        cyclingRinglayer.strokeColor = color.cgColor
+        cyclingRinglayer.position = CGPoint.init(x:  size.width * 0.5, y: size.height * 0.5)
+        cyclingRinglayer.lineWidth = optionalTem.lineWidth
+        
+        let radius = size.width * 0.5
+        let center = CGPoint.init(x: radius, y: radius)
+        
+       let path = UIBezierPath.init(arcCenter: center, radius: radius, startAngle: 3 * (CGFloat.pi / 2), endAngle: 5 * (CGFloat.pi/4), clockwise: true)
+        cyclingRinglayer.path = path.cgPath
+
+        superLayer.addSublayer(cyclingRinglayer)
+        
+        
+        let animation = CABasicAnimation.init()
+        animation.keyPath = "transform.rotation"
+        animation.repeatCount = MAXFLOAT
+        animation.duration = optionalTem.duration
+        animation.fromValue = 0
+        animation.toValue = 2.0 * CGFloat.pi
+        cyclingRinglayer.add(animation, forKey: "cyclingRinglayer")
+        
+    }
+    
+    func removeAnimation() {
+        cyclingRinglayer.removeAnimation(forKey: "cyclingRinglayer")
+    }
+}
+
+
+
+class MLCyclingScale: UIView,MLIndicatorViewProtocol {
+    
+    var realLayer:CAShapeLayer! = nil
+    
+    func setIndicatorAnimationWithSuperLayer(superLayer: CALayer, tintColor color: UIColor, indicatorSize size: CGSize, indicatorOptional optional: MLIndicatorOptional?) {
+        let optionalTem:MLIndicatorOptional
+        if let indicatorOptional = optional {
+            optionalTem = indicatorOptional
+        }else{
+            optionalTem = MLIndicatorOptional.init(lineWidth: 1, reConut: 15, size: CGSize.init(width: 5, height: 5), duration: Double(15) / Double(10))
+        }
+        
+        let reLayer = CAReplicatorLayer.init()
+        reLayer.frame = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+        reLayer.instanceCount = optionalTem.reConut
+        reLayer.backgroundColor = UIColor.clear.cgColor
+        reLayer.position = CGPoint.init(x: size.width * 0.5, y: size.height * 0.5)
+        
+        reLayer.instanceDelay =  optionalTem.duration / CFTimeInterval(optionalTem.reConut)
+        
+        /// 每个复制层的角度值
+         let angle = CGFloat.pi * 2 / CGFloat(optionalTem.reConut)
+         reLayer.instanceTransform = CATransform3DMakeRotation(angle, 0, 0, 1)
+         superLayer.addSublayer(reLayer)
+        
+        addRealLayerToReLayer(layer: reLayer, tintColor: color, indicatorSize: size, indicatorOptional: optionalTem)
+        
+    }
+    
+    
+    func addRealLayerToReLayer(layer:CALayer, tintColor color: UIColor, indicatorSize size: CGSize, indicatorOptional optional: MLIndicatorOptional){
+        
+        self.realLayer = CAShapeLayer.init()
+        self.realLayer.backgroundColor = color.cgColor
+        self.realLayer.bounds = CGRect.init(x: 0, y: 0, width:  optional.itemSize.width, height: optional.itemSize.height)
+        self.realLayer.position = CGPoint.init(x: size.width * 0.5, y: 5)
+        self.realLayer.cornerRadius = self.realLayer.bounds.size.width * 0.5
+        self.realLayer.shouldRasterize = true
+        self.realLayer.rasterizationScale = UIScreen.main.scale
+        self.realLayer.transform = CATransform3DMakeScale(0.1, 0.1, 0.1)
+        
+
+        layer.addSublayer(self.realLayer)
+        
+        let animation = CABasicAnimation.init()
+        animation.keyPath = "transform.scale"
+        animation.fromValue = 1
+        animation.toValue = 0.1
+        animation.duration = optional.duration
+        animation.repeatCount = MAXFLOAT
+        self.realLayer.add(animation, forKey: "animation")
+        
+        
+    }
+    
+    func removeAnimation() {
+        self.realLayer.removeAnimation(forKey: "animation")
+    }
+    
+    
+}
+
+
+class MLTranslation: UIView,MLIndicatorViewProtocol {
+    
+    var realLayer:CALayer! = nil
+    
+    func setIndicatorAnimationWithSuperLayer(superLayer: CALayer, tintColor color: UIColor, indicatorSize size: CGSize, indicatorOptional optional: MLIndicatorOptional?) {
+        
+        let optionalTem:MLIndicatorOptional
+        if let indicatorOptional = optional {
+            optionalTem = indicatorOptional
+        }else{
+            optionalTem = MLIndicatorOptional.init(lineWidth: 1, reConut: 5, size: CGSize.init(width: size.width / 5, height: size.height / 5), duration:0.8)
+        }
+        
+        let reLayer = CAReplicatorLayer.init()
+        reLayer.frame = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+        reLayer.position = CGPoint.init(x: 0, y: 0)
+        reLayer.backgroundColor = UIColor.clear.cgColor
+        reLayer.instanceCount = optionalTem.reConut
+       
+        superLayer.addSublayer(reLayer)
+        
+        reLayer.instanceTransform = CATransform3DMakeTranslation(size.width / CGFloat(optionalTem.reConut), 0, 0)
+         reLayer.instanceDelay =  optionalTem.duration / CFTimeInterval(optionalTem.reConut)
+      
+        addRealLayerToReLayer(layer: reLayer, tintColor: color, indicatorSize: size, indicatorOptional: optionalTem)
+    }
+    
+    
+    
+    func addRealLayerToReLayer(layer:CALayer, tintColor color: UIColor, indicatorSize size: CGSize, indicatorOptional optional: MLIndicatorOptional){
+        
+        self.realLayer = CALayer.init()
+        
+        self.realLayer.bounds = CGRect.init(x: 0, y: 0, width: optional.itemSize.width, height: optional.itemSize.height)
+        // 第一个的中心点位置 optional.itemSize.width * 0.5
+        self.realLayer.position = CGPoint.init(x: optional.itemSize.width * 0.5, y: size.height * 0.5)
+        self.realLayer.backgroundColor = color.cgColor
+
+        self.realLayer.cornerRadius = optional.itemSize.width * 0.5
+        self.realLayer.shouldRasterize = true
+        self.realLayer.rasterizationScale = UIScreen.main.scale
+        self.realLayer.transform = CATransform3DMakeScale(0.2, 0.2, 0.2)
+        
+        
+        layer.addSublayer(self.realLayer)
+        
+        
+       let animation = CABasicAnimation.init()
+        animation.keyPath = "transform.scale"
+        animation.fromValue = 0.2
+        animation.toValue = 1
+        animation.duration =  optional.duration
+        animation.repeatCount = MAXFLOAT
+//        autoreverses动画结束时是否执行逆动画
+        animation.autoreverses = true
+        self.realLayer.add(animation, forKey: "animation")
         
         
         
     }
     
+    func removeAnimation() {
+        self.realLayer.removeAnimation(forKey: "animation")
+    }
     
-    
-
-    
-    
-  
     
     
 }
+
+
+
+
+
+
+
 
 
 
